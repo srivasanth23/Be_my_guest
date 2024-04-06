b33617ee92e16dfafcd66275490d158ac6cacd3a
srivasanth23 <jammulasrivasanth@gmail.com>
2024-04-06 12:28:00 +0530

    Navbar added

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <h1 className="heading">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
