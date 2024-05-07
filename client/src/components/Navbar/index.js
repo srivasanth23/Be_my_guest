e97864b2e1a7efc230e316bfa9eb4bb5f748193c
srivasanth23 <jammulasrivasanth@gmail.com>
2024-05-07 15:54:06 +0530

    Navbar edited

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
 import { IoMenu, IoClose } from "react-icons/io5";
 import "./index.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+  
+
+  const changebackground = () => {
+    if(window.scrollY >= 200){
+      setNavbar(true)
+    }else{
+      setNavbar(false)
+    }
+  }
+
+  window.addEventListener('scroll', changebackground);
+
   return (
-    <nav className="navbar">
+    <nav className={navbar ? "navbar active": "navbar"}>
       <Link to="/">
         <img
           alt="logo"
-          src="https://res.cloudinary.com/dlxjzmiig/image/upload/v1712383406/Red_Typography_Initial_B_Logo_12_m4ipik.png"
+          src="https://res.cloudinary.com/dlxjzmiig/image/upload/v1715073413/Be-my-guest-logo-removebg-preview_m9xwwj.png"
           className="navbar-home-logo"
         />
       </Link>
@@ -43,7 +57,7 @@ const Navbar = () => {
         <Link to="/login" className="link-propety-specific">
           <li className="login">Login</li>
         </Link>
-        <Link to="/register" className="link-propety-specific">
+        <Link to="/register" className={navbar ? "link-propety-specific" : ''}>
           <button className="became-a-host-button">Became A Host</button>
         </Link>
       </ul>
